bc9be704b72691b02de5c361fb3bd0e4b0655479
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-04-14 00:39:21 +0200

    🐛 bug on fake datas for sounds

--- datas/sounds.js
+++ datas/sounds.js
@@ -1,17 +1,17 @@
 // Mock database
 const soundsTest = [
 {
-    "_id": "65df58e59035bde45591b85a",
-    "name": "cloudy",
-    "musique": "https://cdn.pixabay.com/audio/2023/03/20/audio_e560545036.mp3"
+    _id: "65df58e59035bde45591b85a",
+    name: "cloudy",
+    link: "https://cdn.pixabay.com/audio/2023/03/20/audio_e560545036.mp3"
   },
   {
-    "_id": "65df58e59035bde45591b85b",
-    "name": "partlycloudy",
-    "musique": "https://cdn.pixabay.com/audio/2023/03/20/audio_e560545036.mp3"
+    _id: "65df58e59035bde45591b85b",
+    name: "partlycloudy",
+    link: "https://cdn.pixabay.com/audio/2023/03/20/audio_e560545036.mp3"
   },
   {
-    "_id": "65df58e59035bde45591b85c",
-    "name": "sun",
-    "musique": "https://cdn.pixabay.com/audio/2023/09/05/audio_420dd3c465.mp3"
+    _id: "65df58e59035bde45591b85c",
+    name: "sun",
+    link: "https://cdn.pixabay.com/audio/2023/09/05/audio_420dd3c465.mp3"
   }]
